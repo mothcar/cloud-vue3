01c923aefae7a9c2bba0f3f4ee00803cc4bb5fb9
Paul <mothcar@gmail.com>
2022-12-27 12:16:12 +0000

    fix

--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import axios from 'axios'
 
 import './assets/main.css'
 
-const app = createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+
 app.config.globalProperties.axios=axios
+
+app.mount('#app')
